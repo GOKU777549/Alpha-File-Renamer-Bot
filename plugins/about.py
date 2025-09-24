716fc48f50a34581df1327e9be0ad9328c1b4bf6
GOKU777549 <b7774455@gmail.com>
2025-09-25 01:59:28 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,7 +1,12 @@
-
 from pyrogram import Client, filters
 
-
-@Client.on_message(filters.private & filters.command(["about"]))
-async def start(client,message):
-	await message.reply_text("Bot :- @rename_urbot\nCreater :- @mrlokaman\nLanguage :-Python3\nLibrary :- Pyrogram 1.4.16\nServer :- Heroku")
+@Client.on_message(filters.private & filters.command("about"))
+async def about(client, message):
+    text = (
+        "🤖 Bot: @rename_urbot\n"
+        "👤 Creator: @mrlokaman\n"
+        "💻 Language: Python 3\n"
+        "📚 Library: Pyrogram 1.4.16\n"
+        "🖥️ Server: Heroku"
+    )
+    await message.reply_text(text)
