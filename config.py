d2f33c7e08ef97def9375ce61627dda8ca9c1691
GOKU777549 <b7774455@gmail.com>
2025-09-25 00:39:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,15 @@
 import os
 
+ADMIN_ID = [int(x) for x in os.getenv("ADMIN_ID", "7576729648").split()]
+
 class Config:
-    # Telegram
     BOT_TOKEN = os.getenv("BOT_TOKEN", "8302777365:AAGoG2wE6fujqDyMe2oI8eC-wzfwtOIq1e8")
     API_ID = int(os.getenv("API_ID", 21218274))
-    API_HASH = os.getenv("API_HASH", "3474a18b61897c672d315fb330edb213")  # <-- fixed quote
-
-    # Database (MongoDB)
+    API_HASH = os.getenv("API_HASH", "3474a18b61897c672d315fb330edb213")
     MONGO_URL = os.getenv(
         "MONGO_URL",
         "mongodb+srv://sufyan532011:5042@auctionbot.5ms20.mongodb.net/?retryWrites=true&w=majority&appName=AuctionBot"
     )
     DB_NAME = os.getenv("DB_NAME", "FileRenamerBot")
-
-    # Admins (for broadcast/restart)
-    ADMIN_ID = [int(x) for x in os.getenv("ADMIN_ID", "7576729648").split()]
-
-    # Others
     DOWNLOAD_DIR = "./downloads"
-    MAX_FILE_SIZE = int(os.getenv("MAX_FILE_SIZE", 2 * 1024 * 1024 * 1024))  # 2GB default
+    MAX_FILE_SIZE = int(os.getenv("MAX_FILE_SIZE", 2 * 1024 * 1024 * 1024))
