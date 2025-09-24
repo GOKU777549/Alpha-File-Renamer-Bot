93bf981613a7a89c61543978eff38e247159248c
GOKU777549 <b7774455@gmail.com>
2025-09-25 00:01:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 
 class Config:
     # Telegram
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "your-bot-token-here")
-    API_ID = int(os.getenv("API_ID", 12345))
-    API_HASH = os.getenv("API_HASH", "your-api-hash-here")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8302777365:AAGoG2wE6fujqDyMe2oI8eC-wzfwtOIq1e8")
+    API_ID = int(os.getenv("API_ID", 21218274))
+    API_HASH = os.getenv("API_HASH", "3474a18b61897c672d315fb330edb213)
 
     # Database (MongoDB)
-    MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+    MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://sufyan532011:5042@auctionbot.5ms20.mongodb.net/?retryWrites=true&w=majority&appName=AuctionBot")
     DB_NAME = os.getenv("DB_NAME", "FileRenamerBot")
 
     # Admins (for broadcast/restart)
-    ADMINS = [int(x) for x in os.getenv("ADMINS", "123456789").split()]
+    ADMIN_ID = [int(x) for x in os.getenv("ADMINS", "7576729648").split()]
 
     # Others
     DOWNLOAD_DIR = "./downloads"
